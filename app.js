a2ee5b8688b4512e241aee0c7a29e7d6eb1958b0
Yanyan <lisa.renyanyan@gmail.com>
2022-09-11 17:54:46 -0400

    finished

--- app.js
+++ app.js
@@ -26,25 +26,40 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
+  const highSugarArray = fruitsArray.filter((item) => {
+    return item.nutritions.sugar > 15;
+  })
+  console.log(highSugarArray)
 
   // TODO: Create a new HTML element to display your data
-
+  /* const exampleAddElement = () => {
+    // Create a new HTML element and set its properties
+    const newElement = document.createElement('div');
+    newElement.innerHTML = "this text is inside a div";
+  
+    // Append the new element to an existing part of the webpage
+    const existingElement = document.getElementById('fruit-info');
+    existingElement.append(newElement);
+  } */
   // TODO: Append your new element to the page
+  exampleAddElement(highSugarArray)
 
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
-const exampleAddElement = () => {
+const exampleAddElement = (a) => {
   // Create a new HTML element and set its properties
   const newElement = document.createElement('div');
-  newElement.innerHTML = "this text is inside a div";
+  newElement.innerHTML = a.map((item) => {
+    return item.name;
+  })
 
   // Append the new element to an existing part of the webpage
-  const existingElement = document.getElementById('example-id');
+  const existingElement = document.getElementById('fruit-info');
   existingElement.append(newElement);
 }
 
